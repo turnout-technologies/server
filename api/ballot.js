9b5f25275012aa22d59e4049ec8a1b8100a12c3f
hopelessmuffins <7934854+jasondashwang@users.noreply.github.com>
2020-02-03 08:12:47 -0800

    added test get today's ballot route

--- api/ballot.js
+++ api/ballot.js
@@ -1,12 +1,25 @@
 const { Router } = require('express')
+const moment = require('moment')
 const db = require('../db')
 
 const router = Router()
 
 router.get('/today', async (req, res, next) => {
   try {
-    const ballot = await db.collection('ballots').doc('test').get().data()
-    res.json(ballot)
+    if (moment().unix() < moment(1359932400)) res.status(200).send('Too early')
+    else {
+      const snapshot = await db.collection('ballots').where('date', '==', 1359932400).get()
+      // If the query has no results
+      if (snapshot.empty) res.status(500).send('Not Found')
+      else {
+        // Grab the doc in data form
+        let docs = []
+        snapshot.forEach(doc => {
+          docs.push(doc.data())
+        })
+        res.json(docs)
+      }
+    }
   } catch (err) {
     next(err)
   }
