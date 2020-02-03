3283679cac918e028c9c9432d76f9dd53ba7e34a
hopelessmuffins <7934854+jasondashwang@users.noreply.github.com>
2020-02-03 08:27:57 -0800

    test firebase verify id token route for tyler

--- api/index.js
+++ api/index.js
@@ -1,12 +1,18 @@
 const { Router } = require('express')
+const admin = require('../firebase')
 
 const router = Router()
 
 router.use('/ballots', require('./ballot'))
 
-// Expect
-router.post('/hello', (req, res, next) => {
-
+// Expecting token on req.body
+router.post('/hello', async (req, res, next) => {
+  try {
+    let decodedToken = await admin.auth().verifyIdToken(req.body.token)
+    res.status(200).send(decodedToken.uid)
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = router
