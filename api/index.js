6aab8a78762cf382e797496530c0a4a1aead1d79
hopelessmuffins <7934854+jasondashwang@users.noreply.github.com>
2020-02-04 22:45:41 -0800

    added auth check in middleware

--- api/index.js
+++ api/index.js
@@ -6,7 +6,14 @@ const router = Router()
 // Validate request token. If validation fails, gets passed onto error handling endware
 router.use(async (req, res, next) => {
   try {
-    const token = req.header('Authorization').replace('Bearer', '').trim()
+    const authHeader = req.header('Authorization')
+    if (!authHeader) {
+      const err = new Error('Invalid Authorization Header')
+      err.status = 401
+      throw err
+    }
+
+    const token = authHeader.replace('Bearer', '').trim()
     req.decodedToken = await admin.auth().verifyIdToken(token)
     req.token = token
     next()
