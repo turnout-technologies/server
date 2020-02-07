794975f3976569cfa515c4774fbbc0b3c5353366
hopelessmuffins <7934854+jasondashwang@users.noreply.github.com>
2020-02-06 17:13:10 -0800

    always grabs the 6:00pm EST ballot of that day

--- api/ballots.js
+++ api/ballots.js
@@ -6,14 +6,14 @@ const router = Router()
 
 router.use((req, res, next) => {
   //set the start and end times for today EST
-  const start = moment.tz("America/New_York").format("YYYY-MM-DD") + " 18:00";
-  const end = moment.tz("America/New_York").format("YYYY-MM-DD") + " 22:00";
+  const start = moment.tz("America/New_York").format("YYYY-MM-DD") + " 18:00"
+  const end = moment.tz("America/New_York").format("YYYY-MM-DD") + " 22:00"
 
   //convert the times to moments so we can do a compare
-  const startMoment = moment.tz(start, "America/New_York");
-  const endMoment = moment.tz(end, "America/New_York");
+  const startMoment = moment.tz(start, "America/New_York")
+  const endMoment = moment.tz(end, "America/New_York")
 
-  const isBetween = moment.tz("America/New_York").isBetween(startMoment, endMoment);
+  const isBetween = moment.tz("America/New_York").isBetween(startMoment, endMoment)
 
   if (isBetween || process.env.NODE_ENV === 'development') {
     next()
@@ -26,15 +26,24 @@ router.use((req, res, next) => {
 
 router.get('/today', async (req, res, next) => {
   try {
-    const doc = await db.collection('ballots').doc('live').get()
-    if (!doc.exists) res.status(500).send('Not Found')
+    const targetDate = moment.tz(moment.tz("America/New_York").format("YYYY-MM-DD") + " 18:00", "America/New_York")
+
+    const snapshot = await db.collection('ballots').where('date', '==', targetDate.unix()).get()
+    if (snapshot.empty) res.status(500).send('Not Found')
     else {
+      const docs = [];
       // Grab the doc in data form
-      const data = doc.data()
-      // Normalize the data
+      snapshot.forEach(doc => {
+        docs.push(doc.data())
+      })
+
+      if (docs.length !== 1) throw new Error('Multiple Live Ballots')
+
+      const data = docs[0]
       const ballot = {
         id: data.id,
         createdAt: data.createdAt,
+        date: data.date,
         questions: data.questions,
       }
 
