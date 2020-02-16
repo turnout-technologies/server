e9b4f4dd29da41dd2877e85a7980db55bed328a5
hopelessmuffins <7934854+jasondashwang@users.noreply.github.com>
2020-02-16 09:14:37 -0800

    removed ghost logs

--- api/index.js
+++ api/index.js
@@ -14,7 +14,6 @@ router.use(async (req, res, next) => {
     }
 
     const token = authHeader.replace('Bearer', '').trim()
-    console.log(token)
     req.decodedToken = await admin.auth().verifyIdToken(token)
     req.uid = req.decodedToken.uid
     req.token = token
