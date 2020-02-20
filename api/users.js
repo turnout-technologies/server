9b0200106531a75e568d9deec8a9ff613c8c6749
hopelessmuffins <7934854+jasondashwang@users.noreply.github.com>
2020-02-19 18:24:31 -0800

    added leaderboard route

--- api/users.js
+++ api/users.js
@@ -5,6 +5,44 @@ const User = require('../db/models/user')
 
 const router = Router()
 
+router.get('/leaderboard', async (req, res, next) => {
+  try {
+    const snapshot = await db.collection('users').orderBy('points', 'desc').limit(100).get()
+    if (snapshot.empty) throw new Error('No users for leaderboard to show')
+    const users = []
+    // Grab the doc in data form
+    snapshot.forEach(doc => {
+      let user = doc.data()
+      users.push({
+        id: user.id,
+        name: user.name,
+        avatarURL: user.avatarURL,
+        points: user.points,
+      })
+    })
+
+    if (users.find(user => user.id === req.uid)) res.status(200).json({
+      leaderboard: users
+    })
+    else {
+      const doc = await db.collection('users').doc(req.uid).get()
+      const self = doc.data()
+
+      res.status(200).json({
+        leaderboard: users,
+        self: {
+          id: self.id,
+          name: self.name,
+          avatarURL: self.avatarURL,
+          points: self.points,
+        }
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
 router.post('/', async (req, res, next) => {
   try {
     const newUser = new User({
