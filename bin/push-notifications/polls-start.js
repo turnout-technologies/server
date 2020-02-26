eceab0b4b92b8151478fc667f309ca04f08970cc
hopelessmuffins <7934854+jasondashwang@users.noreply.github.com>
2020-02-25 18:41:44 -0800

    preliminary push notifications

---
+++ bin/push-notifications/polls-start.js
@@ -0,0 +1,72 @@
+const { Expo } = require('expo-server-sdk')
+const admin = require('firebase-admin')
+
+let serviceAccount = require('../config/serviceAccountKey.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
+
+const db = admin.firestore()
+// Create a new Expo SDK client
+let expo = new Expo()
+
+const retrieveVoters = async () => {
+  const snapshot = await db.collection('users').get()
+  const users = []
+  // Grab the doc in data form
+  snapshot.forEach(doc => {
+    let user = doc.data()
+    if (user.pushToken) users.push(user.pushToken)
+  })
+
+  return users
+}
+
+const sendNotifications = async () => {
+  try {
+
+    const pushTokens = await retrieveVoters()
+    // Create the messages that you want to send to clents
+    let messages = []
+    for (let pushToken of pushTokens) {
+      // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+      // Check that all your push tokens appear to be valid Expo push tokens
+      if (!Expo.isExpoPushToken(pushToken)) {
+        console.error(`Push token ${pushToken} is not a valid Expo push token`);
+        continue;
+      }
+
+      // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications)
+      messages.push({
+        to: pushToken,
+        sound: 'default',
+        priority: 'high',
+        body: 'Polls are open!',
+        channelId: 'poll-notifications',
+      })
+    }
+
+    // TODO
+    // The Expo push notification service accepts batches of notifications so
+    // that you don't need to send 1000 requests to send 1000 notifications. We
+    // recommend you batch your notifications to reduce the number of requests
+    // and to compress them (notifications with similar content will get
+    // compressed).
+    let chunks = expo.chunkPushNotifications(messages)
+    let tickets = []
+    // Send the chunks to the Expo push notification service. There are
+    // different strategies you could use. A simple one is to send one chunk at a
+    // time, which nicely spreads the load out over time:
+    for (let chunk of chunks) {
+      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+      console.log(ticketChunk);
+      tickets.push(...ticketChunk);
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+sendNotifications()
