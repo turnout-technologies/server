31691c09858d9777d4cd82041a8ec256763178ad
hopelessmuffins <7934854+jasondashwang@users.noreply.github.com>
2020-02-28 22:12:00 -0800

    verbose logging

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const router = Router()
 router.use(async (req, res, next) => {
   try {
     const authHeader = req.header('Authorization')
+    console.error('Authorization Detected')
     if (!authHeader) {
       const err = new Error('Invalid Authorization Header')
       err.status = 401
@@ -16,6 +17,7 @@ router.use(async (req, res, next) => {
     const token = authHeader.replace('Bearer', '').trim()
     req.decodedToken = await admin.auth().verifyIdToken(token)
     .catch(() => {
+      console.error('Bad Token Detected')
       const err = new Error('Invalid Authorization Token')
       err.status = 401
       throw err
