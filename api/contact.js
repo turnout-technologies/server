aa30cefe60de9cf035ca75f7c62ca7c81b31665c
hopelessmuffins <7934854+jasondashwang@users.noreply.github.com>
2020-02-29 11:34:59 -0800

    allow filename for contact

--- api/contact.js
+++ api/contact.js
@@ -5,22 +5,23 @@ const moment = require('moment')
 
 const db = require('../db')
 
-
 const router = Router()
 const contactSchema = Joi.object({
   type: Joi.string().valid('bug', 'happy', 'sad', 'suggestion', 'question_idea').required(),
-  message: Joi.string().min(1).required()
+  message: Joi.string().min(1).required(),
+  filename: Joi.string().allow('')
 })
 
 router.post('/', async (req, res, next) => {
   try {
-    const { type, message } = req.body
-    await contactSchema.validateAsync({ type, message });
+    const { type, message, filename } = req.body
+    await contactSchema.validateAsync({ type, message, filename });
     const docId = uuid()
 
     await db.collection('contact').doc(docId).create({
       type,
       message,
+      filename,
       uid: req.uid,
       createdAt: moment().unix(),
     })
